d9e05a4c34596ab4d3e03fca91c25a98245899b6
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2016-11-21 15:40:23 +0000

    Add initial model for addresses

--- landavailability/api/models.py
+++ landavailability/api/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    # Describe an instance of an address
+
+    uprn = models.CharField(max_length=100)
+    address_line_1 = models.CharField(max_length=255, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=255, blank=True, null=True)
+    address_line_3 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    county = models.CharField(max_length=255, blank=True, null=True)
+    postcode = models.CharField(max_length=50, blank=True, null=True)
+    country_code = models.CharField(max_length=50, blank=True, null=True)
+    point = models.PointField()
