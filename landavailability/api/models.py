7710db90cb75c0d8f1b88849941bf158aa55a671
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2016-11-21 16:44:45 +0000

    Add index to uprn field and rewrite migration

--- landavailability/api/models.py
+++ landavailability/api/models.py
@@ -4,7 +4,7 @@ from django.contrib.gis.db import models
 class Address(models.Model):
     # Describe an instance of an address
 
-    uprn = models.CharField(max_length=100)
+    uprn = models.CharField(db_index=True, max_length=100)
     address_line_1 = models.CharField(max_length=255, blank=True, null=True)
     address_line_2 = models.CharField(max_length=255, blank=True, null=True)
     address_line_3 = models.CharField(max_length=255, blank=True, null=True)
