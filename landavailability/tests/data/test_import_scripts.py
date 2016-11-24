e3525d49d804ed57da88fbf79d40e8b0cc460631
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2016-11-24 10:45:16 +0000

    Add initial empty test

---
+++ landavailability/tests/data/test_import_scripts.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from landavailability.api.management.commands.import_addresses.py import (
+    Command as AddressesCommand
+)
+
+
+class TestAddressCommand(TestCase):
+    def test_import_addresses_process_row(self):
+        pass
