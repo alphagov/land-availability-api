2a49c5efa77a047bff47941bf2d9cd0ba8660d3b
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2016-11-24 12:00:04 +0000

    Add test for import addresses process_row()

--- landavailability/tests/data/test_import_scripts.py
+++ landavailability/tests/data/test_import_scripts.py
@@ -1,9 +1,18 @@
 from unittest import TestCase
+import pytest
 from api.management.commands.import_addresses import (
     Command as AddressesCommand
 )
+from api.models import Address
 
 
 class TestAddressCommand(TestCase):
+    @pytest.mark.django_db
     def test_import_addresses_process_row(self):
-        pass
+        address_row = [
+            "3225845", "10091033016", "Bolton Chapel",
+            "Bolton Village Main Street", "", "Bolton", "Northumberland",
+            "NE66 2EE", "GB", "55.4168769443259", "-1.83356713993623"]
+
+        AddressesCommand().process_row(address_row)
+        self.assertEqual(Address.objects.count(), 1)
