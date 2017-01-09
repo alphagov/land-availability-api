20b91bdb2f733b420cdc92f050fee63678cce5b9
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-01-09 11:07:51 +0000

    Add TrainStopSerializer

--- landavailability/api/serializers.py
+++ landavailability/api/serializers.py
@@ -1,4 +1,4 @@
-from .models import BusStop
+from .models import BusStop, TrainStop
 from rest_framework import serializers
 from django.contrib.gis.geos import GEOSGeometry
 import json
@@ -40,3 +40,43 @@ class BusStopSerializer(serializers.ModelSerializer):
 
         bus_stop.save()
         return bus_stop
+
+
+class TrainStopSerializer(serializers.ModelSerializer):
+    # this extra field is used to specify the srid geo format
+    srid = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = TrainStop
+        fields = '__all__'
+
+        # We want to handle duplicated entries manually so we remove the
+        # unique validator
+        extra_kwargs = {
+            'atcode_code': {
+                'validators': [],
+            }
+        }
+
+    def create(self, validated_data):
+        atcode_code = validated_data['atcode_code']
+
+        try:
+            train_stop = TrainStop.objects.get(atcode_code=atcode_code)
+        except TrainStop.DoesNotExist:
+            train_stop = TrainStop()
+
+        train_stop.atcode_code = validated_data.get('atcode_code')
+        train_stop.naptan_code = validated_data.get('naptan_code')
+        train_stop.point = GEOSGeometry(
+                validated_data.get('point').geojson,
+                srid=validated_data.get('srid'))
+        train_stop.name = validated_data.get('name')
+        train_stop.main_road = validated_data.get('main_road')
+        train_stop.side_road = validated_data.get('side_road')
+        train_stop.type = validated_data.get('type')
+        train_stop.nptg_code = validated_data.get('nptg_code')
+        train_stop.local_reference = validated_data.get('local_reference')
+
+        train_stop.save()
+        return train_stop
