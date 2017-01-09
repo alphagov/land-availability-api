2329f57ba1955be3b1645163b16f5a06657939cf
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-01-09 13:51:54 +0000

    Add API endpoint to POST TrainStop

--- landavailability/api/urls.py
+++ landavailability/api/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(
         r'^busstop/$',
         views.BusStopCreateView.as_view(), name='busstop-create'),
+    url(
+        r'^trainstop/$',
+        views.TrainStopCreateView.as_view(), name='trainstop-create'),
 ]
--- landavailability/api/views.py
+++ landavailability/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
-from .models import BusStop
-from .serializers import BusStopSerializer
+from .models import BusStop, TrainStop
+from .serializers import BusStopSerializer, TrainStopSerializer
 from django.contrib.gis.geos import GEOSGeometry
 
 
@@ -17,6 +17,21 @@ class BusStopCreateView(APIView):
         if serializer.is_valid():
             bus_stop = serializer.save()
             bus_stop.update_close_locations()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TrainStopCreateView(APIView):
+    permission_classes = (IsAdminUser, )
+
+    def post(self, request, format=None):
+        serializer = TrainStopSerializer(data=request.data)
+
+        if serializer.is_valid():
+            train_stop = serializer.save()
+            train_stop.update_close_locations()
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
