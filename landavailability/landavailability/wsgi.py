47e6ec4f67143a35fd023304672599f352c9cfae
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-01-24 11:50:50 +0000

    Add WhiteNoise support in wsgi file

--- landavailability/landavailability/wsgi.py
+++ landavailability/landavailability/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "landavailability.settings")
 
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
