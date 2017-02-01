c836ea1cd7aca9491fc3051b68dee35d4e6ab832
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-02-01 14:02:20 +0000

    Get SECRET_KEY from env variable

--- landavailability/landavailability/settings.py
+++ landavailability/landavailability/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0wy^52c3#$u718caz^$_fsao3q%l+p42n*x%x-&(kq$x=o^f&*'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
