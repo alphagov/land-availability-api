001564f6050faf00c300e0514918456d141d286d
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-02-03 14:32:10 +0000

    Add land-availability-api.herokuapp.com to ALLOWED_HOSTS

--- landavailability/landavailability/settings.py
+++ landavailability/landavailability/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['land-availability-api.herokuapp.com']
 
 
 # Application definition
